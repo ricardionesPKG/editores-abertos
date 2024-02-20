6a481630810d396d356da2f1ced3927644550755
ricardo fonseca <ricardo.fonseca@escola.pr.gov.br>
2024-02-20 10:40:09 -0300

    editores abertos

---
+++ script.js
@@ -0,0 +1,9 @@
+let idade = 17
+
+if (idade>16){
+    console.log(`${idade} anos -> maior de idade!`)
+} else if (idade == 16){
+    console.log(`$(idade) anos -> está no limite!`)
+} else{
+    console.log(`${idade} anos -> menor de idade!`)
+}
